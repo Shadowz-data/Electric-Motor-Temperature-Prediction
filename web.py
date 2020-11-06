b46cad0d17ddb7bff2278024471df299e88ca45c
Shadowz-data <74040059+Shadowz-data@users.noreply.github.com>
2020-11-06 12:56:50 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, request, render_template
+import joblib
+
+with open(f'joblib_model1.pkl', 'rb') as f:
+    model = joblib.load(f)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    f = request.files['csv']
+    features = pd.read_csv(f)
+    data = [np.array(features)]
+    prediction = model.predict(data)
+
+    output = round(prediction[0], 6)
+
+    return render_template('home.html', prediction_text = "Motor Temperature = [ {} ]".format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
